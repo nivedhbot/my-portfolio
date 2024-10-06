5230de20d35b330468aadc7a268cec86c4506ba6
Nivedh manoj <149414134+nivedhbot@users.noreply.github.com>
2024-10-06 11:43:34 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,60 @@
+function hamburg() {
+  const navbar = document.querySelector(".dropdown");
+  navbar.style.transform = "translateY(0px)";
+}
+
+function cancel() {
+  const navbar = document.querySelector(".dropdown");
+  navbar.style.transform = "translateY(-500px)";
+}
+
+// Typewriter Effect
+
+const texts = ["DEVELOPER", "SOFTWARE ENGINEER"];
+
+let speed = 100;
+const textElements = document.querySelector(".typewriter-text");
+
+let textIndex = 0;
+let charcterIndex = 0;
+
+function typeWriter() {
+  if (charcterIndex < texts[textIndex].length) {
+    textElements.innerHTML += texts[textIndex].charAt(charcterIndex);
+    charcterIndex++;
+    setTimeout(typeWriter, speed);
+  } else {
+    setTimeout(eraseText, 1000);
+  }
+}
+
+function eraseText() {
+  if (textElements.innerHTML.length > 0) {
+    textElements.innerHTML = textElements.innerHTML.slice(0, -1);
+    setTimeout(eraseText, 50);
+  } else {
+    textIndex = (textIndex + 1) % texts.length;
+    charcterIndex = 0;
+    setTimeout(typeWriter, 500);
+  }
+}
+
+window.onload = typeWriter;
+
+// SendMail function using EmailJS
+function SendMail() {
+  var params = {
+    from_name: document.getElementById("fullName").value,
+    email_id: document.getElementById("email_id").value,
+    message: document.getElementById("message").value,
+  };
+
+  emailjs
+    .send("service_t5x5htn", "template_dewutgh", params)
+    .then(function (res) {
+      alert("Success! " + res.status);
+    })
+    .catch(function (error) {
+      alert("Failed to send: " + error);
+    });
+}
